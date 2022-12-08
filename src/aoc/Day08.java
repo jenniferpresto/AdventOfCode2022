c2af4290da1b7e699194074bab3df132b14569b6
Jennifer Presto <jpresto@renttherunway.com>
2022-12-08 07:33:07 -0500

    part 2

--- src/aoc/Day08.java
+++ src/aoc/Day08.java
@@ -14,7 +14,7 @@ public class Day08 {
                 data.add(scanner.nextLine());
             }
         } catch (Exception e) {
-            System.out.println("Oops " + e.getMessage());
+            System.out.println("Oops... " + e.getMessage());
         }
 
         int forestWidth = data.get(0).length();
@@ -25,11 +25,7 @@ public class Day08 {
         int[][] tallestVisibleTreeFromTop = new int[forestWidth][forestHeight];
         int[][] tallestVisibleTreeFromBottom = new int[forestWidth][forestHeight];
         int[][] visibleTrees = new int[forestWidth][forestHeight];
-
-//        Arrays.fill(forest, 0);
-
-        //  start with the edges
-//        int totalVisibleTrees = (forestWidth * 2) + (forestHeight * 2) - 4;
+        int[][] scenicScores = new int[forestWidth][forestHeight];
 
         for (int x = 0; x < forestWidth; x++) {
             for (int y = 0; y < forestHeight; y++) {
@@ -40,6 +36,7 @@ public class Day08 {
                 tallestVisibleTreeFromRight[x][y] = -1;
                 tallestVisibleTreeFromTop[x][y] = -1;
                 tallestVisibleTreeFromBottom[x][y] = -1;
+                scenicScores[x][y] = -1;
                 if (x == 0 || y == 0 || x == forestWidth - 1 || y == forestHeight - 1) {
                     visibleTrees[x][y] = treeXY;
                     tallestVisibleTreeFromLeft[x][y] = treeXY;
@@ -50,6 +47,7 @@ public class Day08 {
             }
         }
 
+        //  part 1
         //  visible from the left?
         for (int y = 1; y < forestHeight - 1; y++) {
             for (int x = 1; x < forestWidth; x++) {
@@ -98,14 +96,59 @@ public class Day08 {
             }
         }
 
+        //  calculate scenic scores for each tree
+        for (int x = 0; x < forestWidth; x++) {
+            for (int y = 0; y < forestHeight; y++) {
+                int score = calculateScenicScore(forest, x, y);
+                scenicScores[x][y] = score;
+            }
+        }
 
-
-//        printForest(forest);
-//        System.out.println("****");
-        printForest(visibleTrees);
-        System.out.println(("Total visible trees: " + countVisibleTrees(visibleTrees)));
-//        System.out.println(totalVisibleTrees);
+        //  printForest(visibleTrees);
+        //  printForest(scenicScores);
+        System.out.println(("Part 1: Total visible trees: " + countVisibleTrees(visibleTrees)));
+        System.out.println("Part 2: Highest scenic score: " + getHighestScenicScore(scenicScores));
     }
+
+    static int calculateScenicScore(int[][] forest, int x, int y) {
+        int visibleTreesLeft = 0;
+        int visibleTreesRight = 0;
+        int visibleTreesUp = 0;
+        int visibleTreesDown = 0;
+        if (x == 1 && y == 4) {
+            int jennifer = 9;
+        }
+        //  left
+        for (int i = x-1; i > -1; i--) {
+            visibleTreesLeft++;
+            if (forest[x][y] <= forest[i][y]) {
+                break;
+            }
+        }
+        //  right
+        for (int i = x+1; i < forest[0].length; i++) {
+            visibleTreesRight++;
+            if (forest[x][y] <= forest[i][y]) {
+                break;
+            }
+        }
+        //  up
+        for (int i = y-1; i > -1; i--) {
+            visibleTreesUp++;
+            if (forest[x][y] <= forest[x][i]) {
+                break;
+            }
+        }
+        //  down
+        for (int i = y+1; i < forest.length; i++) {
+            visibleTreesDown++;
+            if (forest[x][y] <= forest[x][i]) {
+                break;
+            }
+        }
+        return visibleTreesLeft * visibleTreesRight * visibleTreesUp * visibleTreesDown;
+    }
+
 
     static int countVisibleTrees(int[][] forest) {
         int totalVisibleTrees = 0;
@@ -117,6 +160,18 @@ public class Day08 {
             }
         }
         return totalVisibleTrees;
+    }
+
+    static int getHighestScenicScore(int[][] scoreGrid) {
+        int highestScore = 0;
+        for (int x = 0; x < scoreGrid[0].length; x++) {
+            for (int y = 0; y < scoreGrid.length; y++) {
+                if (scoreGrid[x][y] > highestScore) {
+                    highestScore = scoreGrid[x][y];
+                }
+            }
+        }
+        return highestScore;
     }
     static void printForest(int[][] forest) {
         for (int i = 0; i < forest[0].length; i++) {
