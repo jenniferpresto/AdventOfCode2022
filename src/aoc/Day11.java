f768de174495b9a16b18d85da36ecf89932c420e
Jennifer Presto <jpresto@renttherunway.com>
2022-12-11 09:50:35 -0500

    a little bit of cleanup

--- src/aoc/Day11.java
+++ src/aoc/Day11.java
@@ -11,13 +11,13 @@ import java.util.Scanner;
 public class Day11 {
     public static final boolean PART_1 = false;
     public static final int NUM_ROUNDS = PART_1 ? 20 : 10000;
+    public static long LEAST_COMMON_MULT = 1;
     private static class Monkey {
         final int id;
         long numInspections = 0;
         Queue<Long> items = new LinkedList<>();
         String operation = "";
         int testDivisor = 1;
-        int commonDivisor = 1;
         Monkey trueRecipient;
         Monkey falseRecipient;
 
@@ -27,7 +27,6 @@ public class Day11 {
 
         public long getNumInspections() { return numInspections; }
         public void catchItem(Long item) {
-//            System.out.println("Monkey " + id + " catching item " + item);
             items.add(item);
         }
 
@@ -35,8 +34,8 @@ public class Day11 {
             this.numInspections++;
             //  pick up the item
             Long top = items.remove();
-            if (top > commonDivisor) {
-                top = top % commonDivisor;
+            if (top > LEAST_COMMON_MULT) {
+                top = top % LEAST_COMMON_MULT;
             }
 
             String[] splitOperation = operation.split(" ");
@@ -85,49 +84,31 @@ public class Day11 {
         }
         int currentMonkey = 0;
         for (String line : data) {
-            if (line.startsWith("Monkey")) {
-                continue;
-            }
             String trimmed = line.trim();
+            String[] splitLine = trimmed.split(" ");
             if (trimmed.startsWith("Starting items:")) {
                 String itemStr = trimmed.substring(16);
                 String[] parsedItems = itemStr.split(", ");
                 for (String item : parsedItems) {
                     monkeys.get(currentMonkey).items.add(Long.valueOf(item));
                 }
-            }
-            String[] splitLine = trimmed.split(" ");
-            if (trimmed.startsWith("Operation")) {
+            } else if (trimmed.startsWith("Operation")) {
                 monkeys.get(currentMonkey).operation = line.substring(line.indexOf('=') + 2);
-                continue;
-            }
-            if (trimmed.startsWith("Test")) {
+            } else if (trimmed.startsWith("Test")) {
                 monkeys.get(currentMonkey).testDivisor = Integer.valueOf(splitLine[3]);
-                continue;
-            }
-            if (trimmed.startsWith("If true:")) {
+            } else if (trimmed.startsWith("If true:")) {
                 monkeys.get(currentMonkey).trueRecipient = monkeys.get(Integer.valueOf(splitLine[5]));
-                continue;
-            }
-            if (trimmed.startsWith("If false:")) {
+            } else if (trimmed.startsWith("If false:")) {
                 monkeys.get(currentMonkey).falseRecipient = monkeys.get(Integer.valueOf(splitLine[5]));
-                continue;
-            }
-            if (line.isBlank()) {
+            } else if (line.isBlank()) {
                 currentMonkey++;
             }
         }
 
-        //  get least common multiple of all divisors
-        int commonMult = 1;
+        //  get smallest multiple of all divisors
         for (int i = 0; i < monkeys.size(); i++) {
-            commonMult = getLeastCommonMultiple(commonMult, monkeys.get(i).testDivisor);
+            LEAST_COMMON_MULT = getLeastCommonMultiple(LEAST_COMMON_MULT, monkeys.get(i).testDivisor);
         }
-        System.out.println("Common multiple is :" + commonMult);
-        for (Monkey monkey : monkeys) {
-            monkey.commonDivisor = commonMult;
-        }
-        System.out.println("LCM for 6, 15: " + getLeastCommonMultiple(1, 19));
 
         for (int i = 0; i < NUM_ROUNDS; i++) {
             for (int j = 0; j < monkeys.size(); j++) {
@@ -135,23 +116,24 @@ public class Day11 {
                     monkeys.get(j).inspectOperateTestAndThrowTopItem();
                 }
             }
-            int jennifer = 9;
         }
-//
+
         for (Monkey monkey : monkeys) {
             System.out.println("Monkey " + monkey.id + " inspected items " + monkey.getNumInspections() + " times");
         }
+
         //  sort the monkeys by level of activity
         monkeys.sort(Comparator.comparingLong(Monkey::getNumInspections).reversed());
 
         System.out.println("Level of monkey business: " + (monkeys.get(0).getNumInspections() * monkeys.get(1).getNumInspections()));
     }
 
-    //  no zero or negative values
-    public static int getLeastCommonMultiple(int a, int b) {
-        int higher = a > b ? a : b;
-        int lower = higher == a ? b : a;
-        int lcm = higher;
+    //  no zeroes or negative values
+    //  turned out to be unnecessary for this input, but hey ho!
+    public static long getLeastCommonMultiple(long a, long b) {
+        long higher = a > b ? a : b;
+        long lower = higher == a ? b : a;
+        long lcm = higher;
         while (lcm  % lower != 0) {
             lcm += higher;
         }
