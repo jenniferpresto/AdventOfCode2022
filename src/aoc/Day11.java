63d33b4f510c9acc8311bd904ae35d3d7de8e08b
Jennifer Presto <jpresto@renttherunway.com>
2022-12-11 08:12:33 -0500

    part 1

--- src/aoc/Day11.java
+++ src/aoc/Day11.java
@@ -2,16 +2,19 @@ package aoc;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Day11 {
+    public static final int NUM_ROUNDS = 20;
     private static class Monkey {
         final int id;
+        int numInspections = 0;
         Queue<Integer> items = new LinkedList<>();
-        String operation;
+        String operation = "";
         int testDivisor = 1;
         Monkey trueRecipient;
         Monkey falseRecipient;
@@ -20,11 +23,13 @@ public class Day11 {
             this.id = id;
         }
 
+        public int getNumInspections() { return numInspections; }
         public void catchItem(Integer item) {
             items.add(item);
         }
 
         public void inspectOperateTestAndThrowTopItem() {
+            this.numInspections++;
             //  pick up the item
             Integer top = items.remove();
 
@@ -44,13 +49,12 @@ public class Day11 {
             //  test and throw
             Monkey recipientMonkey = top % testDivisor == 0 ? trueRecipient : falseRecipient;
             recipientMonkey.catchItem(top);
-            int jennifer = 9;
         }
     }
 
     public static void main(String[] args) {
         List<String> data = new ArrayList<>();
-        try (final Scanner scanner = new Scanner(new File("testData/Day11.txt"))) {
+        try (final Scanner scanner = new Scanner(new File("data/Day11.txt"))) {
             while (scanner.hasNext()) {
                 data.add(scanner.nextLine());
             }
@@ -106,7 +110,18 @@ public class Day11 {
             }
         }
 
-        monkeys.get(0).inspectOperateTestAndThrowTopItem();
+        for (int i = 0; i < NUM_ROUNDS; i++) {
+            for (int j = 0; j < monkeys.size(); j++) {
+                while(monkeys.get(j).items.size() > 0) {
+                    monkeys.get(j).inspectOperateTestAndThrowTopItem();
+                }
+            }
+            int jennifer = 9;
+        }
 
+        //  sort the monkeys by level of activity
+        monkeys.sort(Comparator.comparingInt(Monkey::getNumInspections).reversed());
+
+        System.out.println("Level of monkey business: " + (monkeys.get(0).getNumInspections() * monkeys.get(1).getNumInspections()));
     }
 }
